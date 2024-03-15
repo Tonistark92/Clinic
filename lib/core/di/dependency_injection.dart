09df683ebd16d1ad22b4bc1409a0835134a01e65
Islam Hani Riad <hanyislam270@gmail.com>
2024-03-15 14:59:06 +0200

    added signup api service added signup repo added signup cubit added signup regex & reouting added signup widgets added signup screen

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -4,6 +4,8 @@ import 'package:get_it/get_it.dart';
 
 import '../../features/login/data/repos/login_repo.dart';
 import '../../features/login/domain/cubit/login_cubit.dart';
+import '../../features/signin/data/repos/sign_up_repo.dart';
+import '../../features/signin/domain/cubit/sign_up_cubit.dart';
 import '../networking/api_service.dart';
 import '../networking/dio_factory.dart';
 
@@ -18,4 +20,7 @@ Future<void> setupGetIt() async {
   // login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
   getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+    // signup
+  getIt.registerLazySingleton<SignupRepo>(() => SignupRepo(getIt()));
+  getIt.registerFactory<SignupCubit>(() => SignupCubit(getIt()));
 }
--- lib/core/theming/mycolors.dart
+++ lib/core/theming/mycolors.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 class ColorsManager {
   static const Color mainBlue = Color(0xFF247CFF);
   static const Color darkBlue = Color(0xFF242424);
